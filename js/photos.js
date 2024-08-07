b11954510bd76437132773b0595dd53d5ab86b2e
Pav01 <jigr09@mail.ru>
2024-08-07 20:30:42 +0300

    добавление docFrag

--- js/photos.js
+++ js/photos.js
@@ -2,13 +2,16 @@ import {getPictures} from './data.js';
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const picturesContainer = document.querySelector('.pictures');
-const similarPicture = getPictures();
 
-similarPicture.forEach((picture) => {
+const similarPicture = getPictures();
+const similarListFragment = document.createDocumentFragment();
+
+similarPicture.forEach(({url,likes,comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = picture.url;
-  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
-  pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
-  picturesContainer.appendChild(pictureElement);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  similarListFragment.appendChild(pictureElement);
 });
 
+picturesContainer.appendChild(similarListFragment);
